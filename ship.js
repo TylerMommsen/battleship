6e25ec1c93911c1e8e30eb81daa79cfda3740bb3
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-08-30 21:25:42 +0300

    Created ship class

---
+++ ship.js
@@ -0,0 +1,17 @@
+export default class Ship {
+    constructor(length, timesHit, hasSunk) {
+        this.length = length;
+        this.timesHit = 0;
+        this.hasSunk = false;
+    }
+
+    hit() {
+        this.timesHit += 1;
+    }
+
+    isSunk() {
+        if (this.timesHit === this.length) {
+            this.hasSunk = true;
+        }
+    }
+}
